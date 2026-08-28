27814ddf962489407c345b883b2e1cf7a2e70399
LOGESHWARANSP <logeshwaran25082005@gmail.com>
2026-08-28 10:56:29 +0530

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height==null || height.length==0)
+        return 0;
+        
+int res=0;
+int left=0;
+int right=height.length-1;
+int leftmax=height[left];
+int rightmax=height[right];
+while(left<right){
+ if(leftmax<rightmax){
+    left++;
+    leftmax=Math.max(leftmax,height[left]);
+    res+=leftmax-height[left];
+ }
+ else{
+ right--;
+ rightmax=Math.max(rightmax,height[right]);
+ res+=rightmax-height[right];
+}
+}
+return res;
+    
+
+
+    }
+}
